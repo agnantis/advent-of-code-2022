dcc198c0a0a1b91713ce940fda925d7fd0238716
Kostas Agnantis <kagnadis@gmail.com>
2022-12-03 22:33:17 +0200

    Day 1 - Rafactoring

--- src/AoC/Utils.hs
+++ src/AoC/Utils.hs
@@ -3,12 +3,7 @@ module AoC.Utils (splitWhen) where
 splitWhen :: (a -> Bool) -> [a] -> [[a]]
 splitWhen = go []
  where
-  go yss _ [] = yss
-  go [] p (x:xs)
-    | p x = go [] p xs
-    | otherwise   = go [[x]] p xs
-  go l@(ys:yss) p (x:xs)
-    | p x = go ([]:l) p xs
-    | otherwise  = go ((x:ys):yss) p xs
-
-
+  go ys _ [] = [ys]
+  go ys p (x:xs)
+    | p x = ys:(go [] p xs)
+    | otherwise  = go (x:ys) p xs
