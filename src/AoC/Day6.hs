8ed7a61dff42a55df0d78862127d00eb29c2118b
Kostas Agnantis <kagnadis@gmail.com>
2022-12-06 22:18:09 +0200

    Day 6 - Star 1

---
+++ src/AoC/Day6.hs
@@ -0,0 +1,32 @@
+module AoC.Day6 where
+
+import Control.Arrow ((&&&))
+import Data.List (nub)
+
+type Input = [Char]
+type Output = Int
+
+
+allDifferent :: Eq a => Int -> [a] -> Bool
+allDifferent n = (== n) . length . nub . take n
+
+searchForUnique :: Eq a => Int -> Int -> [a] -> Int
+searchForUnique n _  [] = error $ "Unable to find " <> show n <> " successive unique elements"
+searchForUnique n indx xs
+  | allDifferent n xs = indx
+  | otherwise         = searchForUnique n (indx+1) (tail xs)
+
+---
+
+fstStar :: Input -> Output
+fstStar = searchForUnique 4 4
+
+sndStar :: Input -> Output
+sndStar = undefined
+
+---
+
+mainDay6 :: IO ()
+mainDay6 = do
+  input <- readFile "src/input/day6"
+  print . (fstStar &&& sndStar) $ input
